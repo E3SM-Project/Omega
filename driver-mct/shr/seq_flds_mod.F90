njeffery/E3SM

Adds omega-fluxes option for frazil coupling in sea ice
Updates Icepack submodule to frazil coupling branch
Adds ocean-ice coupling fields:
frazil salt flux (Fioo_frazils)
frazil enthalpy flux (Fioo_frazilh) defined consistent with freezingMeltPotential

To use, set config_frazil_coupling_type to ‘omega-fluxes’
Backwards compatible with mpas-ocean for config_frazil_coupling_type = ‘external’

Stealth

driver-mct/shr/seq_flds_mod.F90
@@ -1602,6 +1602,24 @@ contains
     attname  = 'Fioo_frazil'
     call metadata_set(attname, longname, stdname, units)
 
+    ! Ocean frazil salt flux
+    call seq_flds_add(o2x_fluxes,"Fioo_frazils")
+    call seq_flds_add(x2i_fluxes,"Fioo_frazils")
+    longname = 'Ocean frazil salt flux'
+    stdname  = 'ocean_frazil_ice_salt_flux'
+    units    = 'kg m-2 s-1'
+    attname  = 'Fioo_frazils'
+    call metadata_set(attname, longname, stdname, units)
+
+    ! Ocean frazil enthalpy flux
+    call seq_flds_add(o2x_fluxes,"Fioo_frazilh")
+    call seq_flds_add(x2i_fluxes,"Fioo_frazilh")
+    longname = 'Ocean frazil enthalpy flux'
+    stdname  = 'ocean_frazil_ice_enthalpy_flux'
+    units    = 'W m-2'
+    attname  = 'Fioo_frazilh'
+    call metadata_set(attname, longname, stdname, units)
+
     ! Heat flux from melting
     call seq_flds_add(i2x_fluxes,"Fioi_melth")
     call seq_flds_add(x2o_fluxes,"Fioi_melth")

E3SM-Project/Omega

driver-mct/shr/seq_flds_mod.F90
@@ -1602,23 +1602,23 @@ contains
     attname  = 'Fioo_frazil'
     call metadata_set(attname, longname, stdname, units)
 
-   ! Ocean frazil salt production
-   call seq_flds_add(o2x_fluxes,"Fioo_frazils")
-   call seq_flds_add(x2i_fluxes,"Fioo_frazils")
-   longname = 'Ocean frazil salt production'
-   stdname  = 'ocean_frazil_salt_production'
-   units    = 'kg m-2 s-1'
-   attname  = 'Fioo_frazils'
-   call metadata_set(attname, longname, stdname, units)
+    ! Ocean frazil salt flux
+    call seq_flds_add(o2x_fluxes,"Fioo_frazils")
+    call seq_flds_add(x2i_fluxes,"Fioo_frazils")
+    longname = 'Ocean frazil salt flux'
+    stdname  = 'ocean_frazil_salt_flux'
+    units    = 'kg m-2 s-1'
+    attname  = 'Fioo_frazils'
+    call metadata_set(attname, longname, stdname, units)
 
-   ! Ocean frazil head flux
-   call seq_flds_add(o2x_fluxes,"Fioo_frazilh")
-   call seq_flds_add(x2i_fluxes,"Fioo_frazilh")
-   longname = 'Ocean frazil heat flux'
-   stdname  = 'ocean_frazil_heat_flux'
-   units    = 'W m-2'
-   attname  = 'Fioo_frazilh'
-   call metadata_set(attname, longname, stdname, units)
+    ! Ocean frazil head flux
+    call seq_flds_add(o2x_fluxes,"Fioo_frazilh")
+    call seq_flds_add(x2i_fluxes,"Fioo_frazilh")
+    longname = 'Ocean frazil heat flux'
+    stdname  = 'ocean_frazil_heat_flux'
+    units    = 'W m-2'
+    attname  = 'Fioo_frazilh'
+    call metadata_set(attname, longname, stdname, units)
 
     ! Heat flux from melting
     call seq_flds_add(i2x_fluxes,"Fioi_melth")